077e7ff8d448b54404a1a7e6a9b61482a378deb3
平間洸揮 <hiramakouki@MacBook-Pro.local>
2025-05-08 18:17:51 +0900

    実装02

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,12 @@
 Rails.application.routes.draw do
-  root 'dishes#index'
-  get 'generate', to: 'dishes#generate'
-  get "dishes/index"
-  get "dishes/generate"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'home#index'  # トップページを "dishes#index" に設定
+  get "generate", to: "dishes#generate"
+  get "home/index"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # Health check route
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/*
+  # PWA routes
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
